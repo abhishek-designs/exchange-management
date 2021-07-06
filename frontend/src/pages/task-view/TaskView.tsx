a85f8f6d8ce7fe618e6b916ca49cf007958e9031
Abhishek Kalyane <63503591+abhishek-designs@users.noreply.github.com>
2021-07-06 15:26:22 +0530

    task view screen partially created

--- frontend/src/pages/task-view/TaskView.tsx
+++ frontend/src/pages/task-view/TaskView.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Header from "../../components/layouts/header/Header";
 import {
   IonContent,
@@ -5,21 +6,23 @@ import {
   IonButton,
   IonIcon,
   IonText,
-  IonImg,
+  IonModal,
+  IonLabel,
+  IonInput,
+  IonTextarea,
+  IonSelect,
+  IonSelectOption,
 } from "@ionic/react";
 import { useHistory } from "react-router";
 
 import { arrowBack, pencilOutline, sync } from "ionicons/icons";
-import item1 from "../../assets/img/items/approve-item-1.png";
-import item2 from "../../assets/img/items/approve-item-2.png";
-import item3 from "../../assets/img/items/approve-item-3.png";
-import item4 from "../../assets/img/items/approve-item-4.png";
-import shellLogo from "../../assets/icons/shell.svg";
 
 import "./TaskView.css";
 
-const ApprovalView = () => {
+const TaskView = () => {
   const history = useHistory();
+  // State for the update form modal
+  const [openModal, setModal] = useState<boolean>(false);
 
   return (
     <>
@@ -90,7 +93,10 @@ const ApprovalView = () => {
                     <IonIcon icon={sync} color="primary" />
                   </IonCard>
 
-                  <IonButton className="attend-btn">
+                  <IonButton
+                    className="attend-btn"
+                    onClick={() => setModal(true)}
+                  >
                     <IonIcon icon={pencilOutline} color="light" />
                     <IonText color="light" className="btn-caption">
                       Update Task Status
@@ -102,8 +108,92 @@ const ApprovalView = () => {
           </IonCard>
         </IonCard>
       </IonContent>
+      {/* Modal to update the task status */}
+      <IonModal
+        isOpen={openModal}
+        onDidDismiss={() => setModal(false)}
+        backdropDismiss
+        cssClass="inspection-modal"
+      >
+        <IonCard className="task-update-form-contain">
+          <IonCard className="top-content">
+            <IonCard className="modal-top-header">
+              <IonIcon
+                icon={pencilOutline}
+                color="primary"
+                className="edit-icon"
+              />
+              <IonText color="primary">Update Task Status</IonText>
+            </IonCard>
+            <IonText color="primary" className="modal-header">
+              Inspection Report
+            </IonText>
+          </IonCard>
+          <IonCard className="form-contain">
+            <IonCard className="form-grp">
+              <IonLabel color="primary" className="form-label">
+                New Status
+              </IonLabel>
+              <IonSelect
+                className="status-select"
+                value="in progress"
+                interface="popover"
+              >
+                <IonSelectOption color="primary-light" value="in progress">
+                  In Progress
+                </IonSelectOption>
+              </IonSelect>
+            </IonCard>
+            <IonCard className="form-grp">
+              <IonLabel color="primary" className="form-label">
+                Upload Requirements (.pdf, .docx)
+              </IonLabel>
+              <IonInput
+                type="text"
+                className="input-field"
+                placeholder="Inspection report"
+                required
+              ></IonInput>
+            </IonCard>
+            <IonCard className="form-grp">
+              <IonLabel color="primary" className="form-label">
+                Date of Completion
+              </IonLabel>
+              <IonInput
+                type="date"
+                className="input-field"
+                placeholder="dd/mm/yyyy"
+                required
+              ></IonInput>
+            </IonCard>
+            <IonCard className="form-grp">
+              <IonLabel color="primary" className="form-label">
+                Remarks
+              </IonLabel>
+              <IonTextarea
+                className="input-field"
+                placeholder="Enter Remarks"
+                required
+              ></IonTextarea>
+            </IonCard>
+          </IonCard>
+          <IonCard className="btn-grp">
+            <IonButton
+              color="danger"
+              fill="outline"
+              className="form-btn"
+              onClick={() => setModal(false)}
+            >
+              <IonText color="danger">Cancel</IonText>
+            </IonButton>
+            <IonButton color="primary" className="form-btn">
+              <IonText color="light">Update Status</IonText>
+            </IonButton>
+          </IonCard>
+        </IonCard>
+      </IonModal>
     </>
   );
 };
 
-export default ApprovalView;
+export default TaskView;
